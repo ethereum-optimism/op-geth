d77f8f64c22e64c89fd63d6cfb7b251d85310889
protolambda <proto@protolambda.com>
2022-07-16 01:31:25 +0200

    miner: restore ability to reorg deep as block builder

--- miner/miner.go
+++ miner/miner.go
@@ -41,6 +41,7 @@ import (
 type Backend interface {
 	BlockChain() *core.BlockChain
 	TxPool() *txpool.TxPool
+	StateAtBlock(block *types.Block, reexec uint64, base *state.StateDB, checkLive bool, preferDisk bool) (statedb *state.StateDB, err error)
 }
 
 // Config is the configuration parameters of mining.
