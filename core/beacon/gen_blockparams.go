tuxcanfly/reference-optimistic-geth

WIP: Extend PayloadAttributesV1 with a Transactions field

core/beacon/gen_blockparams.go
@@ -15,14 +15,21 @@ var _ = (*payloadAttributesMarshaling)(nil)
 // MarshalJSON marshals as JSON.
 func (p PayloadAttributesV1) MarshalJSON() ([]byte, error) {
 	type PayloadAttributesV1 struct {
-		Timestamp             hexutil.Uint64 `json:"timestamp"     gencodec:"required"`
-		Random                common.Hash    `json:"random"        gencodec:"required"`
-		SuggestedFeeRecipient common.Address `json:"suggestedFeeRecipient"  gencodec:"required"`
+		Timestamp             hexutil.Uint64  `json:"timestamp"     gencodec:"required"`
+		Random                common.Hash     `json:"random"        gencodec:"required"`
+		SuggestedFeeRecipient common.Address  `json:"suggestedFeeRecipient"  gencodec:"required"`
+		Transactions          []hexutil.Bytes `json:"transactions,omitempty"  gencodec:"optional"`
 	}
 	var enc PayloadAttributesV1
 	enc.Timestamp = hexutil.Uint64(p.Timestamp)
 	enc.Random = p.Random
 	enc.SuggestedFeeRecipient = p.SuggestedFeeRecipient
+	if p.Transactions != nil {
+		enc.Transactions = make([]hexutil.Bytes, len(p.Transactions))
+		for k, v := range p.Transactions {
+			enc.Transactions[k] = v
+		}
+	}
 	return json.Marshal(&enc)
 }
 
@@ -32,6 +39,7 @@ func (p *PayloadAttributesV1) UnmarshalJSON(input []byte) error {
 		Timestamp             *hexutil.Uint64 `json:"timestamp"     gencodec:"required"`
 		Random                *common.Hash    `json:"random"        gencodec:"required"`
 		SuggestedFeeRecipient *common.Address `json:"suggestedFeeRecipient"  gencodec:"required"`
+		Transactions          []hexutil.Bytes `json:"transactions,omitempty"  gencodec:"optional"`
 	}
 	var dec PayloadAttributesV1
 	if err := json.Unmarshal(input, &dec); err != nil {
@@ -49,5 +57,11 @@ func (p *PayloadAttributesV1) UnmarshalJSON(input []byte) error {
 		return errors.New("missing required field 'suggestedFeeRecipient' for PayloadAttributesV1")
 	}
 	p.SuggestedFeeRecipient = *dec.SuggestedFeeRecipient
+	if dec.Transactions != nil {
+		p.Transactions = make([][]byte, len(dec.Transactions))
+		for k, v := range dec.Transactions {
+			p.Transactions[k] = v
+		}
+	}
 	return nil
 }

ethereum-optimism/op-geth

core/beacon/gen_blockparams.go
@@ -15,14 +15,21 @@ var _ = (*payloadAttributesMarshaling)(nil)
 // MarshalJSON marshals as JSON.
 func (p PayloadAttributesV1) MarshalJSON() ([]byte, error) {
 	type PayloadAttributesV1 struct {
-		Timestamp             hexutil.Uint64 `json:"timestamp"     gencodec:"required"`
-		Random                common.Hash    `json:"prevRandao"        gencodec:"required"`
-		SuggestedFeeRecipient common.Address `json:"suggestedFeeRecipient"  gencodec:"required"`
+		Timestamp             hexutil.Uint64  `json:"timestamp"     gencodec:"required"`
+		Random                common.Hash     `json:"prevRandao"        gencodec:"required"`
+		SuggestedFeeRecipient common.Address  `json:"suggestedFeeRecipient"  gencodec:"required"`
+		Transactions          []hexutil.Bytes `json:"transactions,omitempty"  gencodec:"optional"`
 	}
 	var enc PayloadAttributesV1
 	enc.Timestamp = hexutil.Uint64(p.Timestamp)
 	enc.Random = p.Random
 	enc.SuggestedFeeRecipient = p.SuggestedFeeRecipient
+	if p.Transactions != nil {
+		enc.Transactions = make([]hexutil.Bytes, len(p.Transactions))
+		for k, v := range p.Transactions {
+			enc.Transactions[k] = v
+		}
+	}
 	return json.Marshal(&enc)
 }
 
@@ -32,6 +39,7 @@ func (p *PayloadAttributesV1) UnmarshalJSON(input []byte) error {
 		Timestamp             *hexutil.Uint64 `json:"timestamp"     gencodec:"required"`
 		Random                *common.Hash    `json:"prevRandao"        gencodec:"required"`
 		SuggestedFeeRecipient *common.Address `json:"suggestedFeeRecipient"  gencodec:"required"`
+		Transactions          []hexutil.Bytes `json:"transactions,omitempty"  gencodec:"optional"`
 	}
 	var dec PayloadAttributesV1
 	if err := json.Unmarshal(input, &dec); err != nil {
@@ -49,5 +57,11 @@ func (p *PayloadAttributesV1) UnmarshalJSON(input []byte) error {
 		return errors.New("missing required field 'suggestedFeeRecipient' for PayloadAttributesV1")
 	}
 	p.SuggestedFeeRecipient = *dec.SuggestedFeeRecipient
+	if dec.Transactions != nil {
+		p.Transactions = make([][]byte, len(dec.Transactions))
+		for k, v := range dec.Transactions {
+			p.Transactions[k] = v
+		}
+	}
 	return nil
 }