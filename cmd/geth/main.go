4e0e5c0f9905abcf287cbc2e1970aa64a6882e3d
Javed Khan <javed@optimism.io>
2022-08-13 04:54:55 +0530

    txpool: add flag to disable tx gossip (#42)

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -154,6 +154,7 @@ var (
 		utils.MinerNotifyFullFlag,
 		utils.IgnoreLegacyReceiptsFlag,
 		utils.RollupSequencerHTTPFlag,
+		utils.RollupDisableTxPoolGossipFlag,
 		configFileFlag,
 	}, utils.NetworkFlags, utils.DatabasePathFlags)
 
