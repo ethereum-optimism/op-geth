e154d09a4adc29cfb2fca7a9f9f64c413e18b1be
Matthew Slipper <me@matthewslipper.com>
2022-12-02 17:15:25 -0700

    output

--- core/types/transaction_marshalling.go
+++ core/types/transaction_marshalling.go
@@ -19,6 +19,7 @@ package types
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -122,6 +123,8 @@ func (tx *Transaction) UnmarshalJSON(input []byte) error {
 	if err := json.Unmarshal(input, &dec); err != nil {
 		return err
 	}
+
+	fmt.Println(string(input))
 
 	// Decode / verify fields according to transaction type.
 	var inner TxData
