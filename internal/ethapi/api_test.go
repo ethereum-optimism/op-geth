b838f824f6fbf25339c9aa7c439b0139407a2359
Adrian Sutton <adrian@oplabs.co>
2023-02-13 10:49:31 +1000

    Use table test and further improve coverage

--- internal/ethapi/api_test.go
+++ internal/ethapi/api_test.go
@@ -12,52 +12,85 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_newRPCTransaction_UnmarshalDepositTx_RoundTrip(t *testing.T) {
-	tx := types.NewTx(&types.DepositTx{
-		SourceHash:          common.HexToHash("0x1234"),
-		IsSystemTransaction: true,
-		Mint:                big.NewInt(34),
-	})
-	rpcTx := newRPCTransaction(tx, common.Hash{}, uint64(12), uint64(1), big.NewInt(0), &params.ChainConfig{})
-	json, err := json.Marshal(rpcTx)
-	require.NoError(t, err, "marshalling failed")
-	parsed := &types.Transaction{}
-	err = parsed.UnmarshalJSON(json)
-	require.NoError(t, err, "unmarshal failed")
-}
-
-func Test_newRPCTransaction_UnmarshalDepositTx_NilValues(t *testing.T) {
-	tx := types.NewTx(&types.DepositTx{
-		SourceHash:          common.HexToHash("0x1234"),
-		IsSystemTransaction: true,
-		Mint:                big.NewInt(34),
-	})
-	rpcTx := newRPCTransaction(tx, common.Hash{}, uint64(12), uint64(1), big.NewInt(0), &params.ChainConfig{})
-	rpcTx.V = nil
-	rpcTx.R = nil
-	rpcTx.S = nil
-	rpcTx.GasPrice = nil
-	json, err := json.Marshal(rpcTx)
-	require.NoError(t, err, "marshalling failed")
-	parsed := &types.Transaction{}
-	err = parsed.UnmarshalJSON(json)
-	require.NoError(t, err, "unmarshal failed")
-}
-
-func Test_newRPCTransaction_UnmarshalDepositTx_ZeroValues(t *testing.T) {
-	tx := types.NewTx(&types.DepositTx{
-		SourceHash:          common.HexToHash("0x1234"),
-		IsSystemTransaction: true,
-		Mint:                big.NewInt(34),
-	})
-	rpcTx := newRPCTransaction(tx, common.Hash{}, uint64(12), uint64(1), big.NewInt(0), &params.ChainConfig{})
-	rpcTx.V = (*hexutil.Big)(common.Big0)
-	rpcTx.R = (*hexutil.Big)(common.Big0)
-	rpcTx.S = (*hexutil.Big)(common.Big0)
-	rpcTx.GasPrice = (*hexutil.Big)(common.Big0)
-	json, err := json.Marshal(rpcTx)
-	require.NoError(t, err, "marshalling failed")
-	parsed := &types.Transaction{}
-	err = parsed.UnmarshalJSON(json)
-	require.NoError(t, err, "unmarshal failed")
+func Test_UnmarshalRpcDepositTx(t *testing.T) {
+	tests := []struct {
+		name     string
+		modifier func(tx *RPCTransaction)
+		valid    bool
+	}{
+		{
+			name:     "Unmodified",
+			modifier: func(tx *RPCTransaction) {},
+			valid:    true,
+		},
+		{
+			name: "Zero Values",
+			modifier: func(tx *RPCTransaction) {
+				tx.V = (*hexutil.Big)(common.Big0)
+				tx.R = (*hexutil.Big)(common.Big0)
+				tx.S = (*hexutil.Big)(common.Big0)
+				tx.GasPrice = (*hexutil.Big)(common.Big0)
+			},
+			valid: true,
+		},
+		{
+			name: "Nil Values",
+			modifier: func(tx *RPCTransaction) {
+				tx.V = nil
+				tx.R = nil
+				tx.S = nil
+				tx.GasPrice = nil
+			},
+			valid: true,
+		},
+		{
+			name: "Non-Zero GasPrice",
+			modifier: func(tx *RPCTransaction) {
+				tx.GasPrice = (*hexutil.Big)(big.NewInt(43))
+			},
+			valid: false,
+		},
+		{
+			name: "Non-Zero V",
+			modifier: func(tx *RPCTransaction) {
+				tx.V = (*hexutil.Big)(big.NewInt(43))
+			},
+			valid: false,
+		},
+		{
+			name: "Non-Zero R",
+			modifier: func(tx *RPCTransaction) {
+				tx.R = (*hexutil.Big)(big.NewInt(43))
+			},
+			valid: false,
+		},
+		{
+			name: "Non-Zero S",
+			modifier: func(tx *RPCTransaction) {
+				tx.S = (*hexutil.Big)(big.NewInt(43))
+			},
+			valid: false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			tx := types.NewTx(&types.DepositTx{
+				SourceHash:          common.HexToHash("0x1234"),
+				IsSystemTransaction: true,
+				Mint:                big.NewInt(34),
+			})
+			rpcTx := newRPCTransaction(tx, common.Hash{}, uint64(12), uint64(1), big.NewInt(0), &params.ChainConfig{})
+			test.modifier(rpcTx)
+			json, err := json.Marshal(rpcTx)
+			require.NoError(t, err, "marshalling failed: %w", err)
+			println(string(json))
+			parsed := &types.Transaction{}
+			err = parsed.UnmarshalJSON(json)
+			if test.valid {
+				require.NoError(t, err, "unmarshal failed: %w", err)
+			} else {
+				require.Error(t, err, "unmarshal should have failed but did not")
+			}
+		})
+	}
 }
