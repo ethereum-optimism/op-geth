8fd09c48f03d032e4ce47fee550fbdb6dee74204
Adrian Sutton <adrian@oplabs.co>
2023-02-23 13:48:53 +1000

    Include 0 values for v,r,s and gasPrice on deposit transactions (#48)
    When serializing deposit transactions for RPC responses, provide 0 values for v, r, s and gasPrice instead of null to improve compatibility with existing parsing code.

--- internal/ethapi/api_test.go
+++ internal/ethapi/api_test.go
@@ -12,6 +12,25 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestNewRPCTransactionDepositTx(t *testing.T) {
+	tx := types.NewTx(&types.DepositTx{
+		SourceHash:          common.HexToHash("0x1234"),
+		IsSystemTransaction: true,
+		Mint:                big.NewInt(34),
+	})
+	got := newRPCTransaction(tx, common.Hash{}, uint64(12), uint64(1), big.NewInt(0), &params.ChainConfig{})
+	// Should provide zero values for unused fields that are required in other transactions
+	require.Equal(t, got.GasPrice, (*hexutil.Big)(big.NewInt(0)), "newRPCTransaction().GasPrice = %v, want 0x0", got.GasPrice)
+	require.Equal(t, got.V, (*hexutil.Big)(big.NewInt(0)), "newRPCTransaction().V = %v, want 0x0", got.V)
+	require.Equal(t, got.R, (*hexutil.Big)(big.NewInt(0)), "newRPCTransaction().R = %v, want 0x0", got.R)
+	require.Equal(t, got.S, (*hexutil.Big)(big.NewInt(0)), "newRPCTransaction().S = %v, want 0x0", got.S)
+
+	// Should include deposit tx specific fields
+	require.Equal(t, *got.SourceHash, tx.SourceHash(), "newRPCTransaction().SourceHash = %v, want %v", got.SourceHash, tx.SourceHash())
+	require.Equal(t, *got.IsSystemTx, tx.IsSystemTx(), "newRPCTransaction().IsSystemTx = %v, want %v", got.IsSystemTx, tx.IsSystemTx())
+	require.Equal(t, got.Mint, (*hexutil.Big)(tx.Mint()), "newRPCTransaction().Mint = %v, want %v", got.Mint, tx.Mint())
+}
+
 func TestUnmarshalRpcDepositTx(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -83,7 +102,6 @@ func TestUnmarshalRpcDepositTx(t *testing.T) {
 			test.modifier(rpcTx)
 			json, err := json.Marshal(rpcTx)
 			require.NoError(t, err, "marshalling failed: %w", err)
-			println(string(json))
 			parsed := &types.Transaction{}
 			err = parsed.UnmarshalJSON(json)
 			if test.valid {
