c7a0653a90802983591e3787823237fa63f49bf3
Adrian Sutton <adrian@oplabs.co>
2023-05-24 13:33:35 +1000

    core/types: Include overhead in the L1GasUsed for receipts.

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -536,7 +536,8 @@ func (rs Receipts) DeriveFields(config *params.ChainConfig, hash common.Hash, nu
 				if !txs[i].IsDepositTx() {
 					gas := txs[i].RollupDataGas().DataGas(time, config)
 					rs[i].L1GasPrice = l1Basefee
-					rs[i].L1GasUsed = new(big.Int).SetUint64(gas)
+					// GasUsed reported in receipt should include the overhead
+					rs[i].L1GasUsed = new(big.Int).Add(new(big.Int).SetUint64(gas), overhead)
 					rs[i].L1Fee = L1Cost(gas, l1Basefee, overhead, scalar)
 					rs[i].FeeScalar = feeScalar
 				}
