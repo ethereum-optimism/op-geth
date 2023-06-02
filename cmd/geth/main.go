1f276eb6ec43347691fa4f1fd9267e583fc28032
Adrian Sutton <adrian@oplabs.co>
2023-06-02 11:02:50 +1000

    core/txpool: Support journaling remote transactions with --txpool.journalremotes

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -80,6 +80,7 @@ var (
 		utils.TxPoolLocalsFlag,
 		utils.TxPoolNoLocalsFlag,
 		utils.TxPoolJournalFlag,
+		utils.TxPoolJournalRemotesFlag,
 		utils.TxPoolRejournalFlag,
 		utils.TxPoolPriceLimitFlag,
 		utils.TxPoolPriceBumpFlag,
