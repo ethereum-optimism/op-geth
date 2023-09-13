60b827bfc2deb74d72b46cda1777cd824f445ae1
protolambda <proto@protolambda.com>
2023-09-13 12:43:29 +0200

    params: superchain version formatting, bytes8 instead of uint64 build attribute

--- params/superchain_test.go
+++ params/superchain_test.go
@@ -9,7 +9,7 @@ type HumanProtocolVersion struct {
 	VersionType         uint8
 	Major, Minor, Patch uint32
 	Prerelease          uint32
-	Build               uint64
+	Build               [8]byte
 }
 
 type ComparisonCase struct {
@@ -20,43 +20,43 @@ type ComparisonCase struct {
 func TestProtocolVersion_Compare(t *testing.T) {
 	testCases := []ComparisonCase{
 		{
-			A:   HumanProtocolVersion{0, 2, 1, 1, 1, 0},
-			B:   HumanProtocolVersion{0, 1, 2, 2, 2, 0},
+			A:   HumanProtocolVersion{0, 2, 1, 1, 1, [8]byte{}},
+			B:   HumanProtocolVersion{0, 1, 2, 2, 2, [8]byte{}},
 			Cmp: AheadMajor,
 		},
 		{
-			A:   HumanProtocolVersion{0, 1, 2, 1, 1, 0},
-			B:   HumanProtocolVersion{0, 1, 1, 2, 2, 0},
+			A:   HumanProtocolVersion{0, 1, 2, 1, 1, [8]byte{}},
+			B:   HumanProtocolVersion{0, 1, 1, 2, 2, [8]byte{}},
 			Cmp: AheadMinor,
 		},
 		{
-			A:   HumanProtocolVersion{0, 1, 1, 2, 1, 0},
-			B:   HumanProtocolVersion{0, 1, 1, 1, 2, 0},
+			A:   HumanProtocolVersion{0, 1, 1, 2, 1, [8]byte{}},
+			B:   HumanProtocolVersion{0, 1, 1, 1, 2, [8]byte{}},
 			Cmp: AheadPatch,
 		},
 		{
-			A:   HumanProtocolVersion{0, 1, 1, 1, 2, 0},
-			B:   HumanProtocolVersion{0, 1, 1, 1, 1, 0},
+			A:   HumanProtocolVersion{0, 1, 1, 1, 2, [8]byte{}},
+			B:   HumanProtocolVersion{0, 1, 1, 1, 1, [8]byte{}},
 			Cmp: AheadPrerelease,
 		},
 		{
-			A:   HumanProtocolVersion{0, 1, 2, 3, 4, 0},
-			B:   HumanProtocolVersion{0, 1, 2, 3, 4, 0},
+			A:   HumanProtocolVersion{0, 1, 2, 3, 4, [8]byte{}},
+			B:   HumanProtocolVersion{0, 1, 2, 3, 4, [8]byte{}},
 			Cmp: Matching,
 		},
 		{
-			A:   HumanProtocolVersion{0, 3, 2, 1, 5, 3},
-			B:   HumanProtocolVersion{1, 1, 2, 3, 3, 6},
+			A:   HumanProtocolVersion{0, 3, 2, 1, 5, [8]byte{3}},
+			B:   HumanProtocolVersion{1, 1, 2, 3, 3, [8]byte{6}},
 			Cmp: DiffVersionType,
 		},
 		{
-			A:   HumanProtocolVersion{0, 3, 2, 1, 5, 3},
-			B:   HumanProtocolVersion{0, 1, 2, 3, 3, 6},
+			A:   HumanProtocolVersion{0, 3, 2, 1, 5, [8]byte{3}},
+			B:   HumanProtocolVersion{0, 1, 2, 3, 3, [8]byte{6}},
 			Cmp: DiffBuild,
 		},
 		{
-			A:   HumanProtocolVersion{0, 0, 0, 0, 0, 0},
-			B:   HumanProtocolVersion{0, 1, 3, 3, 3, 3},
+			A:   HumanProtocolVersion{0, 0, 0, 0, 0, [8]byte{}},
+			B:   HumanProtocolVersion{0, 1, 3, 3, 3, [8]byte{3}},
 			Cmp: EmptyVersion,
 		},
 	}
@@ -81,6 +81,30 @@ func TestProtocolVersion_Compare(t *testing.T) {
 				if inv != tc.Cmp {
 					t.Fatalf("expected comparison reversed to hold the same, expected %d but got %d", tc.Cmp, inv)
 				}
+			}
+		})
+	}
+}
+func TestProtocolVersion_String(t *testing.T) {
+	testCases := []struct {
+		version  ProtocolVersion
+		expected string
+	}{
+		{ToProtocolVersion([8]byte{}, 0, 0, 0, 0), "v0.0.0"},
+		{ToProtocolVersion([8]byte{}, 0, 0, 0, 1), "v0.0.0-1"},
+		{ToProtocolVersion([8]byte{}, 0, 0, 1, 0), "v0.0.1"},
+		{ToProtocolVersion([8]byte{}, 4, 3, 2, 1), "v4.3.2-1"},
+		{ToProtocolVersion([8]byte{}, 0, 100, 2, 0), "v0.100.2"},
+		{ToProtocolVersion([8]byte{'O', 'P', '-', 'm', 'o', 'd'}, 42, 0, 2, 1), "v42.0.2-1+OP-mod"},
+		{ToProtocolVersion([8]byte{'b', 'e', 't', 'a', '.', '1', '2', '3'}, 1, 0, 0, 0), "v1.0.0+beta.123"},
+		{ToProtocolVersion([8]byte{'a', 'b', 1}, 42, 0, 2, 0), "v42.0.2+0x6162010000000000"}, // do not render invalid alpha numeric
+		{ToProtocolVersion([8]byte{1, 2, 3, 4, 5, 6, 7, 8}, 42, 0, 2, 0), "v42.0.2+0x0102030405060708"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.expected, func(t *testing.T) {
+			got := tc.version.String()
+			if got != tc.expected {
+				t.Fatalf("got %q but expected %q", got, tc.expected)
 			}
 		})
 	}
