cc23a711f8fe7e23e39e35d584a2fa71920978fa
protolambda <proto@protolambda.com>
2023-09-20 14:37:12 +0200

    miner: fix noTxPool early-exit to not block ResolveFull call when tx pool is not used

--- miner/payload_building.go
+++ miner/payload_building.go
@@ -182,13 +182,16 @@ func (w *worker) buildPayload(args *BuildPayloadArgs) (*Payload, error) {
 	// Build the initial version with no transaction included. It should be fast
 	// enough to run. The empty payload can at least make sure there is something
 	// to deliver for not missing slot.
-	empty, _, err := w.getSealingBlock(args.Parent, args.Timestamp, args.FeeRecipient, args.Random, args.Withdrawals, true, args.Transactions, args.GasLimit)
+	empty, emptyFees, err := w.getSealingBlock(args.Parent, args.Timestamp, args.FeeRecipient, args.Random, args.Withdrawals, true, args.Transactions, args.GasLimit)
 	if err != nil {
 		return nil, err
 	}
 	// Construct a payload object for return.
 	payload := newPayload(empty, args.Id())
 	if args.NoTxPool { // don't start the background payload updating job if there is no tx pool to pull from
+		// make sure to make it appear as full, otherwise it will wait indefinitely for payload building to complete.
+		payload.full = empty
+		payload.fullFees = emptyFees
 		return payload, nil
 	}
 
