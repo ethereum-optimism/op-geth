726e59ef790251e8b24ca9efea00bba82069d21c
Michael de Hoog <michael.dehoog@coinbase.com>
2023-10-17 15:23:46 -1000

    Add comment, log, and test

--- consensus/misc/create2deployer.go
+++ consensus/misc/create2deployer.go
@@ -5,10 +5,18 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 )
 
-// create2Deployer is already deployed to Base goerli at 0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2,
+// The original create2deployer contract could not be deployed to Base mainnet at
+// the canonical address of 0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2 due to
+// an accidental nonce increment from a deposit transaction. See
+// https://github.com/pcaversaccio/create2deployer/issues/128 for context. This
+// file applies the contract code to the canonical address manually in the Canyon
+// hardfork.
+
+// create2deployer is already deployed to Base goerli at 0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2,
 // so we deploy it to 0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF1 for hardfork testing purposes
 var create2DeployerAddresses = map[uint64]common.Address{
 	params.BaseGoerliChainID:  common.HexToAddress("0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF1"),
@@ -33,5 +41,6 @@ func EnsureCreate2Deployer(c *params.ChainConfig, timestamp uint64, db vm.StateD
 	if !ok || db.GetCodeSize(address) > 0 {
 		return
 	}
+	log.Info("Setting Create2Deployer code", "address", address, "codeHash", create2DeployerCodeHash)
 	db.SetCode(address, create2DeployerCode)
 }
