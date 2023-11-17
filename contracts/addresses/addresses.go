4025ee14b51f2b087477bbb0923deb932690050d
Karl Bartel <karl.bartel@clabs.co>
2023-11-17 11:06:19 +0100

    Add celo core contract addresses

---
+++ contracts/addresses/addresses.go
@@ -0,0 +1,14 @@
+package addresses
+
+import "github.com/ethereum/go-ethereum/common"
+
+var (
+	CeloTokenAddress            = common.HexToAddress("0x471ece3750da237f93b8e339c536989b8978a438")
+	FeeHandlerAddress           = common.HexToAddress("0xcd437749e43a154c07f3553504c68fbfd56b8778")
+	FeeCurrencyDirectoryAddress = common.HexToAddress("0x71ffbd48e34bdd5a87c3c683e866dc63b8b2a685")
+
+	CeloTokenAlfajoresAddress  = common.HexToAddress("0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9")
+	FeeHandlerAlfajoresAddress = common.HexToAddress("0xEAaFf71AB67B5d0eF34ba62Ea06Ac3d3E2dAAA38")
+
+	AlfajoresChainID uint64 = 44787
+)
