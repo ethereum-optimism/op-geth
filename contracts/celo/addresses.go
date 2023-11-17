37d9421ff242e49b05746af0b387e73f559b0cf5
Karl Bartel <karl.bartel@clabs.co>
2023-11-17 11:06:19 +0100

    Add celo core contract addresses

---
+++ contracts/celo/addresses.go
@@ -0,0 +1,14 @@
+package contracts
+
+import "github.com/ethereum/go-ethereum/common"
+
+var (
+	RegistryAddress                          = common.HexToAddress("0x000000000000000000000000000000000000ce10")
+	GoldTokenAddress                         = common.HexToAddress("0x471ece3750da237f93b8e339c536989b8978a438")
+	FeeHandlerAddress                        = common.HexToAddress("0xcd437749e43a154c07f3553504c68fbfd56b8778")
+	FeeCurrencyWhitelistAddress              = common.HexToAddress("0xbb024e9cdcb2f9e34d893630d19611b8a5381b3c")
+	MentoFeeHandlerSellerAddress             = common.HexToAddress("0x4efa274b7e33476c961065000d58ee09f7921a74")
+	UniswapFeeHandlerSellerAddress           = common.HexToAddress("0xd3aee28548dbb65df03981f0dc0713bfcbd10a97")
+	SortedOraclesAddress                     = common.HexToAddress("0xefb84935239dacdecf7c5ba76d8de40b077b7b33")
+	AddressSortedLinkedListWithMedianAddress = common.HexToAddress("0xED477A99035d0c1e11369F1D7A4e587893cc002B")
+)
