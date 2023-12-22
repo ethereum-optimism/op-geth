9da84a7b0dbdf3ce03ea8d4d30e2ee6a173f192c
Martin Holst Swende <martin@swende.se>
2023-12-22 13:46:27 +0100

    params: begin v1.13.9 release cycle

--- params/version.go
+++ params/version.go
@@ -24,10 +24,10 @@ import (
 
 // Version is the version of upstream geth
 const (
-	VersionMajor = 1        // Major version component of the current release
-	VersionMinor = 13       // Minor version component of the current release
-	VersionPatch = 8        // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionMajor = 1          // Major version component of the current release
+	VersionMinor = 13         // Minor version component of the current release
+	VersionPatch = 9          // Patch version component of the current release
+	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
 // OPVersion is the version of op-geth
