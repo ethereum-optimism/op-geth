603add14176f0e7428ef4322543f53140b602221
Michael de Hoog <michael.dehoog@coinbase.com>
2024-01-30 19:41:25 -1000

    Fix unsigned int overflow

--- core/types/rollup_cost.go
+++ core/types/rollup_cost.go
@@ -334,6 +334,9 @@ func FlzCompressLen(ib []byte) uint32 {
 	}
 	a := uint32(0)
 	ipLimit := uint32(len(ib)) - 13
+	if len(ib) < 13 {
+		ipLimit = 0
+	}
 	for ip := a + 2; ip < ipLimit; {
 		r := uint32(0)
 		d := uint32(0)
