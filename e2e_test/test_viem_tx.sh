f3f5fd8eff453a9256cb10f930e10ea6563ce83c
Karl Bartel <karl.bartel@clabs.co>
2024-04-03 14:53:39 +0200

    Remove unnecessary oracle initialization
    This was necessary for the normal SortedOracle, but the
    MockSortedOracles is already initialized in out dev mode genesis.

--- e2e_test/test_viem_tx.sh
+++ e2e_test/test_viem_tx.sh
@@ -4,8 +4,4 @@ set -eo pipefail
 source shared.sh
 prepare_node
 
-# Add our account as oracle and submit value
-cast send --private-key "$ACC_PRIVKEY" "$SORTED_ORACLES_ADDR" 'addOracle(address token, address oracleAddress)' "$FEE_CURRENCY" "$ACC_ADDR"
-cast send --private-key "$ACC_PRIVKEY" "$SORTED_ORACLES_ADDR" 'report(address token, uint256 value, address lesserKey, address greaterKey)' "$FEE_CURRENCY" 2000000000000000000000000 "$ZERO_ADDRESS" "$ZERO_ADDRESS"
-
 cd js-tests && ./node_modules/mocha/bin/mocha.js test_viem_tx.mjs
