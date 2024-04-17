73fc6a77629349082d9e40dff763cf3daf55735f
Maximilian Langenfeld <15726643+ezdac@users.noreply.github.com>
2024-04-17 12:15:34 +0200

    Add fee currencies to gas price RPC API (#96)
    * Add fee currencies to gas price RPC API
    
    * Fix typo
    
    Co-authored-by: Karl Bartel <karl@karl.berlin>
    
    * Format JS e2e tests
    
    ---------
    
    Co-authored-by: Karl Bartel <karl@karl.berlin>

--- e2e_test/js-tests/send_tx.mjs
+++ e2e_test/js-tests/send_tx.mjs
@@ -1,37 +1,44 @@
 #!/usr/bin/env node
-import { createPublicClient, createWalletClient, http, defineChain } from 'viem'
-import { celoAlfajores } from 'viem/chains'
-import { privateKeyToAccount } from 'viem/accounts'
+import {
+	createPublicClient,
+	createWalletClient,
+	http,
+	defineChain,
+} from "viem";
+import { celoAlfajores } from "viem/chains";
+import { privateKeyToAccount } from "viem/accounts";
 
-const [chainId, privateKey, feeCurrency] = process.argv.slice(2)
+const [chainId, privateKey, feeCurrency] = process.argv.slice(2);
 const devChain = defineChain({
-  ...celoAlfajores,
-  id: parseInt(chainId, 10),
-  name: 'local dev chain',
-  network: 'dev',
-  rpcUrls: {
-    default: {
-      http: ['http://127.0.0.1:8545'],
-    },
-  },
-})
+	...celoAlfajores,
+	id: parseInt(chainId, 10),
+	name: "local dev chain",
+	network: "dev",
+	rpcUrls: {
+		default: {
+			http: ["http://127.0.0.1:8545"],
+		},
+	},
+});
 
-const account = privateKeyToAccount(privateKey) 
+const account = privateKeyToAccount(privateKey);
 const walletClient = createWalletClient({
-  account,
-  chain: devChain,
-  transport: http(),
-})
+	account,
+	chain: devChain,
+	transport: http(),
+});
 
 const request = await walletClient.prepareTransactionRequest({
-  account,
-  to: '0x00000000000000000000000000000000DeaDBeef',
-  value: 2,
-  gas: 90000,
-  feeCurrency,
-  maxFeePerGas: 2000000000n,
-  maxPriorityFeePerGas: 0n,
-})
-const signature = await walletClient.signTransaction(request)
-const hash = await walletClient.sendRawTransaction({ serializedTransaction: signature }) 
-console.log(hash)
+	account,
+	to: "0x00000000000000000000000000000000DeaDBeef",
+	value: 2,
+	gas: 90000,
+	feeCurrency,
+	maxFeePerGas: 2000000000n,
+	maxPriorityFeePerGas: 0n,
+});
+const signature = await walletClient.signTransaction(request);
+const hash = await walletClient.sendRawTransaction({
+	serializedTransaction: signature,
+});
+console.log(hash);
