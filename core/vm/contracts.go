1d6392373edae92ff9abbfcea8ecbfdbdfc00afe
Danyal Prout <me@dany.al>
2024-04-22 17:13:10 -0500

    change from constant to literal

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -1185,7 +1185,7 @@ func (c *p256Verify) Run(input []byte) ([]byte, error) {
 	// Verify the secp256r1 signature
 	if secp256r1.Verify(hash, r, s, x, y) {
 		// Signature is valid
-		return common.LeftPadBytes(common.Big1.Bytes(), 32), nil
+		return common.LeftPadBytes([]byte{1}, 32), nil
 	} else {
 		// Signature is invalid
 		return nil, nil
