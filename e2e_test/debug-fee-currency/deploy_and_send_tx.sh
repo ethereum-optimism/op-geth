306b00d6bdc3a9d716fa0040b43122698d9b63e0
Karl Bartel <karl.bartel@clabs.co>
2024-04-29 08:55:33 +0200

    Update e2e tests to use FeeCurrencyDirectory

--- e2e_test/debug-fee-currency/deploy_and_send_tx.sh
+++ e2e_test/debug-fee-currency/deploy_and_send_tx.sh
@@ -6,7 +6,7 @@ export FEE_CURRENCY=$(\
 	forge create --root . --contracts . --private-key $ACC_PRIVKEY DebugFeeCurrency.sol:DebugFeeCurrency --constructor-args '100000000000000000000000000' $1 $2 --json\
 	| jq .deployedTo -r)
 
-cast send --private-key $ACC_PRIVKEY $FEE_CURRENCY_WHITELIST_ADDR 'addToken(address)' $FEE_CURRENCY --legacy
+cast send --private-key $ACC_PRIVKEY $FEE_CURRENCY_DIRECTORY_ADDR 'setCurrencyConfig(address, address, address, uint256)' $FEE_CURRENCY $FEE_CURRENCY $ORACLE 60000 --legacy
 cast send --private-key $ACC_PRIVKEY $SORTED_ORACLES_ADDR 'setMedianRate(address, uint256)' $FEE_CURRENCY 2000000000000000000000000 --legacy
 echo Fee currency: $FEE_CURRENCY
 
