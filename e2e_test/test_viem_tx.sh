99f0c6a394a3fbdd774ee121c40a37f558a946ee
Karl Bartel <karl.bartel@clabs.co>
2024-05-14 15:49:28 +0200

    e2e_test: exit mocha when hitting timeout

--- e2e_test/test_viem_tx.sh
+++ e2e_test/test_viem_tx.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 source shared.sh
 prepare_node
 
-cd js-tests && ./node_modules/mocha/bin/mocha.js test_viem_tx.mjs
+cd js-tests && ./node_modules/mocha/bin/mocha.js test_viem_tx.mjs --exit
