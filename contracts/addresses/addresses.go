99afa464d86ede9a4928f84f2113be36a6b46747
Karl Bartel <karl.bartel@clabs.co>
2024-06-26 12:32:03 +0200

    Update FeeCurrencyDirectory address
    We will use this address across all Celo networks.

--- contracts/addresses/addresses.go
+++ contracts/addresses/addresses.go
@@ -6,7 +6,7 @@ var (
 	RegistryAddress             = common.HexToAddress("0x000000000000000000000000000000000000ce10")
 	CeloTokenAddress            = common.HexToAddress("0x471ece3750da237f93b8e339c536989b8978a438")
 	FeeHandlerAddress           = common.HexToAddress("0xcd437749e43a154c07f3553504c68fbfd56b8778")
-	FeeCurrencyDirectoryAddress = common.HexToAddress("0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb") // tmp address, real one not known yet
+	FeeCurrencyDirectoryAddress = common.HexToAddress("0x71ffbd48e34bdd5a87c3c683e866dc63b8b2a685")
 
 	CeloTokenAlfajoresAddress  = common.HexToAddress("0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9")
 	FeeHandlerAlfajoresAddress = common.HexToAddress("0xEAaFf71AB67B5d0eF34ba62Ea06Ac3d3E2dAAA38")
