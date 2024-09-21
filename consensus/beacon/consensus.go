29dac81d1abca4f446a3242a192a1768f82fdc3c
protolambda <proto@protolambda.com>
2024-09-21 16:21:09 -0600

    all: withdrawals root in header is reused to commit to OP-Stack L2 withdrawals storage root

--- consensus/beacon/consensus.go
+++ consensus/beacon/consensus.go
@@ -404,8 +404,8 @@ func (beacon *Beacon) FinalizeAndAssemble(chain consensus.ChainHeaderReader, hea
 	header.Root = state.IntermediateRoot(true)
 
 	if chain.Config().IsOptimismHolocene(header.Time) {
-		if body.Withdrawals == nil || len(body.Withdrawals) > 0 { // We verify nil/empty withdrawals in the CL pre-holocene
-			return nil, fmt.Errorf("expected non-nil empty withdrawals operation list in Holocene, but got: %v", body.Withdrawals)
+		if body.Withdrawals == nil || len(body.Withdrawals) > 0 { // We verify nil/empty withdrawals in the CL pre-Isthmus
+			return nil, fmt.Errorf("expected non-nil empty withdrawals operation list in Isthmus, but got: %v", body.Withdrawals)
 		}
 		// State-root has just been computed, we can get an accurate storage-root now.
 		h := state.GetStorageRoot(params.OptimismL2ToL1MessagePasser)
