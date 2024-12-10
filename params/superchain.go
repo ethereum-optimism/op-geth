8e9e1dc029b9ff4b6eb8c2cfb64c3a16a418c87e
Sebastian Stammler <stammler.s@gmail.com>
2024-12-10 19:11:03 +0100

    Prepare Holocene mainnet release (#448)

--- params/superchain.go
+++ params/superchain.go
@@ -11,7 +11,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-var OPStackSupport = ProtocolVersionV0{Build: [8]byte{}, Major: 9, Minor: 0, Patch: 0, PreRelease: 1}.Encode()
+var OPStackSupport = ProtocolVersionV0{Build: [8]byte{}, Major: 9, Minor: 0, Patch: 0, PreRelease: 0}.Encode()
 
 func init() {
 	for id, ch := range superchain.OPChains {
