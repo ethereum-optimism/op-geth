0d73c8f807efbac8e4160c803c8fb3e9ac50961b
bbaktaeho <bbaktaeho@gmail.com>
2025-04-09 17:43:13 +0900

    fix: add txHash to historical trace results

--- eth/tracers/api.go
+++ eth/tracers/api.go
@@ -455,6 +455,9 @@ func (api *API) TraceBlockByNumber(ctx context.Context, number rpc.BlockNumber, 
 			if err != nil {
 				return nil, fmt.Errorf("historical backend error: %w", err)
 			}
+			for i, tx := range block.Transactions() {
+				histResult[i].TxHash = tx.Hash()
+			}
 			return histResult, nil
 		} else {
 			return nil, rpc.ErrNoHistoricalFallback
@@ -478,6 +481,9 @@ func (api *API) TraceBlockByHash(ctx context.Context, hash common.Hash, config *
 			err = api.backend.HistoricalRPCService().CallContext(ctx, &histResult, "debug_traceBlockByHash", hash, config)
 			if err != nil {
 				return nil, fmt.Errorf("historical backend error: %w", err)
+			}
+			for i, tx := range block.Transactions() {
+				histResult[i].TxHash = tx.Hash()
 			}
 			return histResult, nil
 		} else {
