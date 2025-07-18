273eaa993a98e173339d01eda98ac687de295b2a
William Law <williamlaw.wtl@gmail.com>
2025-07-18 12:26:49 -0400

    extend extraData header field

--- params/config.go
+++ params/config.go
@@ -1352,7 +1352,7 @@ func (c *ChainConfig) Rules(num *big.Int, isMerge bool, timestamp uint64) Rules 
 		IsOptimismGranite:  isMerge && c.IsOptimismGranite(timestamp),
 		IsOptimismHolocene: isMerge && c.IsOptimismHolocene(timestamp),
 		IsOptimismIsthmus:  isMerge && c.IsOptimismIsthmus(timestamp),
-		IsOptimismJovian: isMerge && c.IsOptimismJovian(timestamp),
+		IsOptimismJovian:   isMerge && c.IsOptimismJovian(timestamp),
 	}
 }
 
