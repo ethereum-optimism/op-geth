cf064287a9937e7857c48c83d4575edf2318da65
William Law <williamlaw.wtl@gmail.com>
2025-07-18 09:56:07 -0400

    add minBaseFee to superchain/types and Jovian to params/config

--- params/config.go
+++ params/config.go
@@ -1311,7 +1311,7 @@ type Rules struct {
 	IsOptimismBedrock, IsOptimismRegolith                   bool
 	IsOptimismCanyon, IsOptimismFjord                       bool
 	IsOptimismGranite, IsOptimismHolocene                   bool
-	IsOptimismIsthmus                                       bool
+	IsOptimismIsthmus, IsOptimismJovian                     bool
 }
 
 // Rules ensures c's ChainID is not nil.
@@ -1351,6 +1351,7 @@ func (c *ChainConfig) Rules(num *big.Int, isMerge bool, timestamp uint64) Rules 
 		IsOptimismGranite:  isMerge && c.IsOptimismGranite(timestamp),
 		IsOptimismHolocene: isMerge && c.IsOptimismHolocene(timestamp),
 		IsOptimismIsthmus:  isMerge && c.IsOptimismIsthmus(timestamp),
+		IsOptimismJovian: isMerge && c.IsOptimismJovian(timestamp),
 	}
 }
 
