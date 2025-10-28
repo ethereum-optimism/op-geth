cb6e1995ed208133316c12c690ac1e29bdfdd64f
Changwan Park <pcw109550@gmail.com>
2025-10-28 23:21:18 +0900

    eth, cmd: add RollupNetrestrictTxPoolGossipFlag PoC using `NilPool` (#713)
    * Make peer struct not aware of ip net restrict
    
    * Propagate Peer ip info to txpool
    
    * fix

--- eth/protocols/eth/handlers.go
+++ eth/protocols/eth/handlers.go
@@ -437,10 +437,6 @@ func handleNewPooledTransactionHashes(backend Backend, msg Decoder, peer *Peer) 
 	if !backend.AcceptTxs() {
 		return nil
 	}
-	// Check if peer is allowed for transaction gossip
-	if !peer.IsAllowedForTxGossip() {
-		return nil
-	}
 	ann := new(NewPooledTransactionHashesPacket)
 	if err := msg.Decode(ann); err != nil {
 		return err
@@ -456,20 +452,16 @@ func handleNewPooledTransactionHashes(backend Backend, msg Decoder, peer *Peer) 
 }
 
 func handleGetPooledTransactions(backend Backend, msg Decoder, peer *Peer) error {
-	// Check if peer is allowed for transaction gossip
-	if !peer.IsAllowedForTxGossip() {
-		return nil
-	}
 	// Decode the pooled transactions retrieval message
 	var query GetPooledTransactionsPacket
 	if err := msg.Decode(&query); err != nil {
 		return err
 	}
-	hashes, txs := answerGetPooledTransactions(backend, query.GetPooledTransactionsRequest)
+	hashes, txs := answerGetPooledTransactions(backend, query.GetPooledTransactionsRequest, peer)
 	return peer.ReplyPooledTransactionsRLP(query.RequestId, hashes, txs)
 }
 
-func answerGetPooledTransactions(backend Backend, query GetPooledTransactionsRequest) ([]common.Hash, []rlp.RawValue) {
+func answerGetPooledTransactions(backend Backend, query GetPooledTransactionsRequest, peer *Peer) ([]common.Hash, []rlp.RawValue) {
 	// Gather transactions until the fetch or network limits is reached
 	var (
 		bytes  int
@@ -481,7 +473,7 @@ func answerGetPooledTransactions(backend Backend, query GetPooledTransactionsReq
 			break
 		}
 		// Retrieve the requested transaction, skipping if unknown to us
-		encoded := backend.TxPool().GetRLP(hash)
+		encoded := backend.TxPool(peer.Node().IPAddr()).GetRLP(hash)
 		if len(encoded) == 0 {
 			continue
 		}
@@ -495,10 +487,6 @@ func answerGetPooledTransactions(backend Backend, query GetPooledTransactionsReq
 func handleTransactions(backend Backend, msg Decoder, peer *Peer) error {
 	// Transactions arrived, make sure we have a valid and fresh chain to handle them
 	if !backend.AcceptTxs() {
-		return nil
-	}
-	// Check if peer is allowed for transaction gossip
-	if !peer.IsAllowedForTxGossip() {
 		return nil
 	}
 	// Transactions can be processed, parse all of them and deliver to the pool
@@ -519,10 +507,6 @@ func handleTransactions(backend Backend, msg Decoder, peer *Peer) error {
 func handlePooledTransactions(backend Backend, msg Decoder, peer *Peer) error {
 	// Transactions arrived, make sure we have a valid and fresh chain to handle them
 	if !backend.AcceptTxs() {
-		return nil
-	}
-	// Check if peer is allowed for transaction gossip
-	if !peer.IsAllowedForTxGossip() {
 		return nil
 	}
 	// Transactions can be processed, parse all of them and deliver to the pool
--- eth/protocols/eth/peer_test.go
+++ eth/protocols/eth/peer_test.go
@@ -45,7 +45,8 @@ func newTestPeer(name string, version uint, backend Backend) (*testPeer, <-chan 
 	var id enode.ID
 	rand.Read(id[:])
 
-	peer := NewPeer(version, p2p.NewPeer(id, name, nil), net, backend.TxPool()).WithTxGossipNetRestrict(backend.TxGossipNetRestrict())
+	p := p2p.NewPeer(id, name, nil)
+	peer := NewPeer(version, p, net, backend.TxPool(p.Node().IPAddr()))
 	errc := make(chan error, 1)
 	go func() {
 		defer app.Close()
