7222d3a92e8847c34fb9d1c1b5d414046ba952bd
Josh Klopfenstein <git@joshklop.com>
2025-11-13 08:04:57 -0800

    simplify

--- eth/downloader/queue.go
+++ eth/downloader/queue.go
@@ -627,22 +627,8 @@ func (q *queue) DeliverBodies(id string, txLists [][]*types.Transaction, txListH
 				}
 			}
 		}
-
-		if q.opConfig != nil && q.opConfig.IsOptimismJovian(header.Time) {
-			if header.BlobGasUsed == nil {
-				return fmt.Errorf("%w: nil blobGasUsed after Jovian", errInvalidBody)
-			}
-			daFootprint, err := types.CalcDAFootprint(txLists[index])
-			if err != nil {
-				return fmt.Errorf("%w: calculate da footprint: %v", errInvalidBody, err)
-			}
-			if *header.BlobGasUsed != daFootprint {
-				return fmt.Errorf("%w: incorrect da footprint: got %d, want %d", errInvalidBody, *header.BlobGasUsed, daFootprint)
-			}
-			if daFootprint > header.GasLimit {
-				return fmt.Errorf("%w: da footprint %d exceeds block gas limit %d", errInvalidBody, daFootprint, header.GasLimit)
-			}
-		} else {
+		// Jovian changes the interpretation of the BlobGasUsed field.
+		if q.opConfig == nil || !q.opConfig.IsOptimismJovian(header.Time) {
 			if header.BlobGasUsed != nil {
 				if want := *header.BlobGasUsed / params.BlobTxBlobGasPerBlob; uint64(blobs) != want { // div because the header is surely good vs the body might be bloated
 					return errInvalidBody
