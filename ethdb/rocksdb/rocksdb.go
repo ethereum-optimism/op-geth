64d32d17856e7282ff2805a1f6e1acdf85bc25a2
Vui-Chee <vuicheesiew@gmail.com>
2026-01-20 15:58:30 +0800

    try remove build tags

--- ethdb/rocksdb/rocksdb.go
+++ ethdb/rocksdb/rocksdb.go
@@ -16,8 +16,6 @@
 
 // Package pebble implements the key-value database layer based on pebble.
 
-//go:build !js && !wasip1 && rocksdb
-
 // Package rocksdb implements the key-value database layer based on RocksDB.
 package rocksdb
 
--- ethdb/rocksdb/rocksdb_test.go
+++ ethdb/rocksdb/rocksdb_test.go
@@ -15,7 +15,6 @@
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
 // Package pebble implements the key-value database layer based on pebble.
-//go:build !js && !wasip1 && rocksdb
 
 package rocksdb
 
