ae434816607ed95d92a102a3e56e3401dce471b2
Yashvardhan Kukreja <yashvardhan@oplabs.co>
2026-01-21 13:33:56 -0500

    fix: mgas (per block) calculation
    Signed-off-by: Yashvardhan Kukreja <yashvardhan@oplabs.co>

--- core/blockchain_insert.go
+++ core/blockchain_insert.go
@@ -58,7 +58,7 @@ func (st *insertStats) report(chain []*types.Block, index int, snapDiffItems, sn
 		// Assemble the log context and send it to the logger
 		context := []interface{}{
 			"number", end.Number(), "hash", end.Hash(),
-			"blocks", st.processed, "txs", txs, "mgas", float64(st.usedGas) / 1000000,
+			"blocks", st.processed, "txs", txs, "mgas", float64(st.usedGas) * 1000 / 1000000,
 			"elapsed", common.PrettyDuration(elapsed), "mgasps", mgasps,
 		}
 		if timestamp := time.Unix(int64(end.Time()), 0); time.Since(timestamp) > time.Minute {
