37cb07545935821c81871b5ffbfba8c4ef2da094
Sebastian Stammler <seb@oplabs.co>
2026-04-28 10:29:47 +0200

    superchain-registry: bump to cc07e96d91a1647cbce7eef6572098bece1f7fca

--- superchain/superchain_test.go
+++ superchain/superchain_test.go
@@ -12,7 +12,9 @@ func TestGetSuperchain(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, "Mainnet", mainnet.Name)
-	require.Equal(t, common.HexToAddress("0x8062AbC286f5e7D9428a0Ccb9AbD71e50d93b935"), mainnet.ProtocolVersionsAddr)
+	require.Equal(t, common.HexToAddress("0x95703e0982140D16f8ebA6d158FccEde42f04a4C"), mainnet.SuperchainConfigAddr)
+	require.Equal(t, common.HexToAddress("0xA8447329e52F64AED2bFc9E7a2506F7D369f483a"), mainnet.SaferSafesAddr)
+	require.Equal(t, uint64(1764691201), *mainnet.Hardforks.JovianTime)
 	require.EqualValues(t, 1, mainnet.L1.ChainID)
 
 	_, err = GetSuperchain("not a network")
